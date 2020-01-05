4f8152013c91cceec38d7be57488078ff037e3bf
latasuresh <58974827+latasuresh@users.noreply.github.com>
2020-01-06 00:31:56 +0800

    Create test_evpn_login.py

---
+++ automations/browser_tests/test_evpn_login.py
@@ -0,0 +1,44 @@
+from nose.plugins.attrib import attr
+  
+#from automations import spec
+from automations.browsers import Browsers, browsers
+from automations.driver_support import drivers
+from automations.test_case import TestCase
+from automations.utils.log import Log
+
+
+@browsers(Browsers.primary)
+class DashboardLoginTests(TestCase):
+        """Tests dashboard sign in.
+        """
+
+        #def before_run(self):
+        #       self.account_prep.reset.all().run()
+
+        ###################################################################################################################
+
+        @drivers('Agent')
+        def test_login_invalid(self):
+                """Logging in with invalid credentials fails and presents expected error message
+                """
+
+                self.evpn = self.login_evpn(
+                        self.Agent, self.account.owner_email,
+                        self.account.password + 'invalid'
+                )
+
+                if self.evpn != None:
+                        self.evpn.signin.credentials_error_visible()
+
+        ##################################################################################################################
+
+        @drivers('Agent')
+        def test_forgot_password(self):
+                """
+                   verify forgot password
+                """
+
+                self.evpn = self.forgot_password(self.Agent, self.account.owner_email)
+                print "h4"
+                if self.evpn != None:
+                        self.evpn.signin.reset_accout_alert_visible()
